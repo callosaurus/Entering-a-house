397a0ff9f20041390ef2b93a72e2a9746c241066
callosaurus <callumdavies421@gmail.com>
2017-02-15 21:51:32 -0800

    first commit

---
+++ Entering a House/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Entering a House
+//
+//  Created by Callum Davies on 2017-02-15.
+//  Copyright © 2017 Callum Davies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Entering a House/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Entering a House
+//
+//  Created by Callum Davies on 2017-02-15.
+//  Copyright © 2017 Callum Davies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
